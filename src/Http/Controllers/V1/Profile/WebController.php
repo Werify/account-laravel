bc2de110869275419d973670a4fdfca1ff5ce831
Muhammad <hi@trader4.net>
2023-08-22 23:14:19 -0700

    [FIX] Fix localization method redirect url replacements

--- src/Http/Controllers/V1/Profile/WebController.php
+++ src/Http/Controllers/V1/Profile/WebController.php
@@ -3,8 +3,11 @@
 namespace Werify\Account\Laravel\Http\Controllers\V1\Profile;
 
 use Illuminate\Routing\Controller;
+use Werify\Account\Laravel\Enums\V1\Partials\Country;
+use Werify\Account\Laravel\Enums\V1\Profile\Currency;
 use Werify\Account\Laravel\Enums\V1\Profile\DarkMode;
 use Werify\Account\Laravel\Enums\V1\Profile\Language;
+use Werify\Account\Laravel\Enums\V1\Profile\Timezone;
 use Werify\Account\Laravel\Http\Requests\V1\Profile\UpdateRequest;
 use Werify\Account\Laravel\Jobs\V1\Profile\UpdateJob;
 
@@ -14,23 +17,28 @@ class WebController extends Controller
     {
         try {
             $data = [];
+            $url = url()->previous();
             if ($r->has('language')) {
                 $data['language'] = $r->input('language');
+                $url = str_replace(Language::toArray(), $r->input('language'), $url);
             }
             if ($r->has('country')) {
                 $data['country'] = $r->input('country');
+                $url = str_replace(Country::countries, $r->input('country'), $url);
             }
             if ($r->has('timezone')) {
                 $data['timezone'] = $r->input('timezone');
+                $url = str_replace(Timezone::timezones, $r->input('timezone'), $url);
             }
             if ($r->has('currency')) {
                 $data['currency'] = $r->input('currency');
+                $url = str_replace(Currency::currencies, $r->input('currency'), $url);
             }
             if (! empty($data)) {
                 dispatch_sync(new UpdateJob(data: $data));
             }
 
-            return redirect(str_replace(Language::toArray(), $r->input('language'), url()->previous()));
+            return redirect($url);
         } catch (\Exception $e) {
             if (config('waccount.debug')) {
                 throw $e;

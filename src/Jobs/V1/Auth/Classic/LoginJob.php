da950304152cbd34a9d43aec257232057c28759c
Muhammad <hi@trader4.net>
2023-09-28 17:20:29 -0700

    [FIX] Fix LoginJob bug and Update composer dependencies

--- src/Jobs/V1/Auth/Classic/LoginJob.php
+++ src/Jobs/V1/Auth/Classic/LoginJob.php
@@ -26,7 +26,7 @@ class LoginJob extends BaseRequest
             $endpoint = $this->generateApiUrl(config('waccount.api.endpoints.auth.classic.login'));
             $req = $this->post($endpoint, $this->data);
             if ($req->status() === 200) {
-                session()->driver(config('waccount.session.driver'))->put(config('waccount.session.variable')['access_token'], ['access_token' => $req->json()['results']['access_token']]);
+                session()->driver(config('waccount.session.driver'))->put(config('waccount.session.variable'), ['access_token' => $req->json()['results']['access_token']]);
 
                 return $req->json();
             }
